362967166bada6dae064e6fc1755df749f4ae560
Refaldy Bagas Anggana <94216252+refaldy04@users.noreply.github.com>
2023-06-09 15:55:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const segitigaLooping = (n) => {
-  for (let i = 0; i < n; i++) {
-    let int = ''
-    for (let j = n; j > i; j--) {
-      int += j
-    }
-    console.log(int)
-  }
-  return 'finish'
-}
 
-console.log(segitigaLooping(5))
